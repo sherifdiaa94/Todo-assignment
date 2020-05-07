ff8056b4ca29895f5adfc7ddcbeb9a010665b6f2
sherifismail <sherif.diaa@eg.ibm.com>
2020-05-07 20:24:04 +0200

    Creating actions

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.scss';
-import { Button } from "carbon-components-react";
+import { Button,ListItem,OrderedList,Tile,Form, FormGroup,TextInput } from "carbon-components-react";
 
-function App() {
-  return (
-    <div className="App">
-      <Button>Button</Button>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+      super()
+  }
+  render(){
+    return (
+      <div className="App">
+        <Tile className="Title-header">
+          Todo List
+        </Tile>
+        <Form>
+        <FormGroup>
+          <TextInput
+            //helperText="Enter below "
+            id="test2"
+            invalidText="Invalid error message."
+            placeholder="Enter here"
+          />
+        </FormGroup>
+        <Button>
+          Submit
+        </Button>
+        <OrderedList className="List">
+              <ListItem>First todo</ListItem>
+              <ListItem>Ordered List level 1</ListItem>
+              <ListItem>Ordered List level 1</ListItem>
+        </OrderedList>
+        </Form>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Button>Button</Button>
+//     </div>
+//   );
+// }
 
 export default App;
