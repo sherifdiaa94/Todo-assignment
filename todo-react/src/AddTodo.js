e8243afe53dca3a67a11b23505d4c55c55a10c79
sherifismail <sherif.diaa@eg.ibm.com>
2020-05-08 20:54:45 +0200

    Now can add and remove item to the todo list

--- todo-react/src/AddTodo.js
+++ todo-react/src/AddTodo.js
@@ -4,13 +4,32 @@ import { Button,ListItem,OrderedList,Tile,Form, FormGroup,TextInput } from "carb
 class AddTodo extends Component{
     constructor(){
         super()   
-        this.handleSubmit=this.handleSubmit.bind(this);     
+        this.handleSubmit=this.handleSubmit.bind(this);  
+        this.handleChange=this.handleChange.bind(this);  
+        this.handleClick=this.handleClick.bind(this);
     }
-
-
+    getInitialState (){
+      return { input: '' };
+      //e.target.value=""
+    }
+    handleChange(e){
+      this.setState({ input: e.target.value});
+    }
+    handleClick(e) {
+      const todoitem= this.state.input;
+      console.log(todoitem);
+      const newtodo={
+          id: Number(new Date()),
+          item:todoitem
+      }
+      if (todoitem){
+          this.props.addtodo(newtodo)
+          this.setState({ input: "" });
+      }
+    }
     handleSubmit(event){
       event.preventDefault();  //To stop the reloading of the page when pressing submit
-
+      console.log(event.target)
       const todoitem= event.target.elements.todo.value;
       console.log(todoitem);
       const newtodo={
@@ -23,18 +42,19 @@ class AddTodo extends Component{
       }
   }
     render(){
+      //onSubmit={this.handleSubmit}
         return (
             <div>
             <Form onSubmit={this.handleSubmit}>
             <FormGroup>
-              <TextInput
+              <TextInput onChange={this.handleChange}
                 id="test2"
                 invalidText="Invalid error message."
                 placeholder="Enter here"
                 name="todo"
               />
             </FormGroup>
-            <Button onClick={this.handleSubmit}>
+            <Button onClick={this.handleClick}>
               Submit
             </Button>
             </Form>
