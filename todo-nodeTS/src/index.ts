885fd70fa24c96c58243677aac83540a298f8e82
sherifismail <sherif.diaa@eg.ibm.com>
2020-05-11 17:05:34 +0200

    Added DELETE and PATCH requests to new node app

--- todo-nodeTS/src/index.ts
+++ todo-nodeTS/src/index.ts
@@ -35,7 +35,7 @@ function Middleware1(request: any, response: express.Response, next:NextFunction
     }else{
       console.log("Not Found")
     }
-  console.log(`${request.method} ${request.path}`);
+  //console.log(`${request.method} ${request.path}`);
   var fs = require('fs');
   // var jsonObj = JSON.parse(request);
   // console.log(jsonObj);
@@ -103,12 +103,12 @@ app.use(Middleware1);
 function Middleware2(request:any, response: express.Response, next:NextFunction) {
   if (request.path!=="/todoApp/api/health"){
     if((JSON.stringify(request.headers['csrf-token'])) && (JSON.stringify(request.headers['x-gateway-apikey']))){
-      console.log("Found")
+      //console.log("Found")
       request.headerFound="Found"
       //response.Headers("Found")
       //return "Found"
     }else{
-      console.log("Not Found")
+      //console.log("Not Found")
       request.headerFound="NotFound"
       //response.json({"Headers":"Not found"})
       //return {"Headers":"Not found"}
@@ -145,18 +145,64 @@ app.get('*', function(req, res){
 });
 
 app.post('/todoApp/api/todos', function (req:any,res){
-  console.log("Inside post"+req)
+  //console.log("Inside post"+req)
   if (req.headerFound==="Found"){
     todoList.push(req.body);
     res.json(todoList);
     }else{
       res.json({"Headers":"Not found"})
      }
-//   todoList.push(req.body);
-//   res.json(todoList);
 });
 
 
+app.delete('/todoApp/api/todos/:id', function (req:any,res){
+  //console.log("Inside delete"+req.headerFound)
+  if (req.headerFound==="Found"){
+    if(todoList.length!==0){
+      var indexof = todoList.findIndex(obj => obj.id==req.params.id);
+      if (indexof<0){
+        res.send('Invalid Item');
+        }
+      else{
+            //console.log(todoList[indexof])
+            const index=req.params.id
+          if (indexof > -1) {
+            todoList.splice(indexof, 1);
+          }
+          res.json(todoList);
+        }
+      }
+      else{
+        res.json(todoList);
+      }
+    }else{
+      res.json({"Headers":"Not found"})
+    }
+
+  
+});
+
+
+app.patch('/todoApp/api/todos/:id', function (req:any,res){
+
+  if (req.headerFound==="Found"){
+    if(todoList.length!==0){
+      var indexof = todoList.findIndex(obj => obj.id==req.params.id);
+      if (indexof<0){
+        res.send('Invalid Item');
+        }
+      else{
+            todoList[indexof].completed=req.body.completed
+            res.json(todoList);
+        }
+      }
+      else{
+        res.json(todoList);
+      }
+    }else{
+      res.json({"Headers":"Not found"})
+    }
+});
 
 
 app.listen(81);
