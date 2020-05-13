5b86b57795092731cf6f0cced34f93e7e6c35652
sherifismail <sherif.diaa@eg.ibm.com>
2020-05-13 15:07:02 +0200

    Added JEST test for health API

--- todo-nodeTS-Jest/src/testing-jest/index.test.ts
+++ todo-nodeTS-Jest/src/testing-jest/index.test.ts
@@ -1,8 +1,20 @@
 const app = require('../index')
+const supertest = require('supertest')
+const request = supertest(app)
+
+
 
 
 
 it('Testing to see if Jest works', () => {
     expect(1).toBe(1)
   })
- 
+
+  describe('Testing Node App', () => {
+    it('Testing Health API', async done => {
+      const response = await request.get('/todoApp/api/health')
+      expect(response.status).toBe(200)
+      expect(response.body.status).toBe('Node server is up and running on port 81')
+      done()
+      })  
+  })
